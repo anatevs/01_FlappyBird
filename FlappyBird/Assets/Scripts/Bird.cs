916d46139fa769e44865642cc05abf4aede4ef4e
anatevs <anatevs@gmail.com>
2025-01-17 18:26:09 +0300

    bird input and collision

--- FlappyBird/Assets/Scripts/Bird.cs
+++ FlappyBird/Assets/Scripts/Bird.cs
@@ -1,14 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace GameCore
 {
+    [RequireComponent(typeof(PlayerInput), typeof(Rigidbody2D))]
     public class Bird : MonoBehaviour
     {
+        public event Action OnRoundEnded;
+
+        [SerializeField]
+        private float speed = 5f;
+
+        private Rigidbody2D _rigidbody;
+
+        private void OnEnable()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
+
         private void OnClick()
         {
-            Debug.Log("click");
+            _rigidbody.velocity = Vector2.up * speed;
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            Debug.Log($"collided with {collision.transform.name}");
+            OnRoundEnded?.Invoke();
         }
     }
 }
